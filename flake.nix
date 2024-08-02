225ddfde5e1086c164096c698b288d581163ff34
Mattias Matthiesen <mattmatthi@gmail.com>
2024-08-02 13:49:05 +0200

    chore: init

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { self', pkgs, ... }:
+        let
+          python = pkgs.python311;
+          libraries = with pkgs; [
+            stdenv.cc.cc
+            libffi
+            zlib
+            libuv
+            cairo
+          ];
+          LIBRARY_PATH = if pkgs.stdenv.isDarwin then "DYLD_LIBRARY_PATH" else "LD_LIBRARY_PATH";
+        in
+        {
+          devenv.shells.default = {
+            packages = (
+              with pkgs;
+              [
+                maturin
+                quarto
+                python.pkgs.jupyter
+              ]
+            );
+            languages.python = {
+              enable = true;
+              package = python;
+              uv = {
+                enable = true;
+                sync = {
+                  enable = true;
+                  allExtras = true;
+                };
+              };
+            };
+            env = {
+              ${LIBRARY_PATH} = "${with pkgs; lib.makeLibraryPath libraries}";
+              UV_PYTHON = "${python}/bin/python";
+            };
+            enterShell = ''
+              echo "🔬 Driven nickelate project shell"
+            '';
+          };
+        };
+    };
+}
